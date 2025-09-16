9681f0ea959f3519574d46c41f91e9d93cb763c3
SomnusAude <shirin.da@phystech.edu>
2025-09-16 13:08:19 +0300

    Add .env

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
+import 'dotenv/config'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+    const port = Number(process.env.PORT)
     const app = await NestFactory.create(AppModule)
-    await app.listen(3000)
-    console.log('Service has been started')
+    await app.listen(port)
+    console.log(`Service has been started on port ${port}`)
 }
 bootstrap()
