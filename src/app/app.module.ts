497b150405c7c6e1ddea5109a1abf85809ca2852
SomnusAude <shirin.da@phystech.edu>
2025-09-17 21:27:44 +0300

    Imlp redis queue logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { FusionBrainModule } from 'src/bussiness-logic/fusion-brain-api/fusion-brain-api.module'
 import { ImageModule } from 'src/bussiness-logic/images/images.module'
 import { PrismaModule } from 'src/bussiness-logic/prisma/prisma.module'
+import { QueueModule } from 'src/bussiness-logic/queue/queue.module'
 
 @Module({
-    imports: [ImageModule, PrismaModule, FusionBrainModule],
+    imports: [ImageModule, PrismaModule, FusionBrainModule, QueueModule],
     controllers: [],
     providers: [],
 })
